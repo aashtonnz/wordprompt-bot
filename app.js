7785030239a62ff22fb38ac0452a2cbd081ce5d4
Andrew Ashton <aashtonnz@gmail.com>
2019-12-28 10:42:43 +1300

    Fix interval bug

--- app.js
+++ app.js
@@ -6,36 +6,22 @@ const name = config.get("username");
 const password = config.get("password");
 const intervalMs = 1000 * Number(config.get("intervalSec"));
 
-const login = async () => {
-  const body = JSON.stringify({ name, password });
-  try {
-    const res = await axios.post("/auth", body);
-    return res.data.token;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const createPrompt = async content => {
-  const body = JSON.stringify({ content });
-  try {
-    return await axios.post("/prompts", body);
-  } catch (error) {
-    console.log(error);
-  }
+const prompt = () => {
+  const index = Math.floor(Math.random() * prompts.length);
+  axios
+    .post("/auth", { name, password })
+    .then(res => {
+      axios.defaults.headers.common["x-auth-token"] = res.data.token;
+      console.log("Token updated");
+      axios
+        .post("/prompts", JSON.stringify({ content: prompts[index] }))
+        .then(() => console.log(`Prompt ${index} created`))
+        .catch(console.error);
+    })
+    .catch(console.error);
 };
 
 (async () => {
-  let token = await login();
-  console.log(token);
-  axios.defaults.headers.common["x-auth-token"] = token;
-  setInterval(() => {
-    const index = Math.floor(Math.random() * prompts.length);
-    token = login()
-      .then(() => "Token updated")
-      .catch(console.error);
-    createPrompt(prompts[index])
-      .then(() => console.log(`Prompt ${index} created`))
-      .catch(console.error);
-  }, intervalMs);
+  prompt();
+  setInterval(prompt, intervalMs);
 })();
